b1470d075e9d6a7997404e8513b6837726eda330
Ardilaaa12 <ardilarosliani12@gmail.com>
2024-09-24 13:19:15 +0700

    middleware customer + admin + verif + login

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\IsCustomer;
+use App\Http\Middleware\IsAdmin;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,16 +22,18 @@ Route::apiResource('/orderDetail', App\Http\Controllers\Api\OrderDetailControlle
 Route::apiResource('/rekening', App\Http\Controllers\Api\RekeningController::class);
 Route::apiResource('/payment', App\Http\Controllers\Api\PaymentController::class);
 Route::apiResource('/navbar', App\Http\Controllers\Api\NavbarController::class);
-Route::apiResource('/section', App\Http\Controllers\Api\SectionController::class);
 Route::apiResource('/content', App\Http\Controllers\Api\ContentController::class);
 Route::apiResource('/review', App\Http\Controllers\Api\ReviewController::class);
-Route::apiResource('/shipping', App\Http\Controllers\Api\ShippingControllers::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/verify', [AuthController::class, 'verify'])->name('verify');
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-// route untuk register, login dan logout
-Route::prefix('account')->group(function () {
+Route::middleware(['auth:sanctum', IsCustomer::class])->group(function () {
+    Route::apiResource('/section', App\Http\Controllers\Api\SectionController::class);
+});
+
+Route::middleware(['auth:sanctum', IsAdmin::class])->group(function () {
+    Route::apiResource('/shipping', App\Http\Controllers\Api\ShippingControllers::class);
 });
